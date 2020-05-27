ae32d8954047665b2e6c833fd219b2e2c8082b51
Josiah Perisho <863370@cognizant.com>
2020-05-27 17:22:55 -0500

    init

---
+++ src/main/java/eg/securityangular/SecurityAngularApplication.java
@@ -0,0 +1,13 @@
+package eg.securityangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityAngularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eg/securityangular/SecurityAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package eg.securityangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
